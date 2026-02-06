0b110fe8daad0098fb89bd0e1a2a1db947dd9d21
Ramisés Reis <ramisesreis@outlook.com>
2026-02-06 08:29:35 -0300

    first commit

---
+++ print_input.py
@@ -0,0 +1,8 @@
+nome = input("Qual é o seu nome? ")
+idade = input("Quantos anos você tem? ")
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep=" # ", end=" ... \n")
+print(nome, idade, sep=" # ")
+
---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Ramisés"
+idade = 39
+
+nome, idade = "Otávio", 7
+
+print(nome, idade)
+
+limite_saque_diario = 5000.00
+
+BRAZILIAM_STATES = ['PE', 'SP', 'RJ', 'MG', 'BA', 'RS', 'SC', 'PR', 'GO', 'DF']
+
+print(BRAZILIAM_STATES)
