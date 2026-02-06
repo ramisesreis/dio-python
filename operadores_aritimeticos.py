d1407fdc0b5d3b307572775000200db48ab0b32f
Ramisés Reis <ramisesreis@outlook.com>
2026-02-06 08:33:56 -0300

    add operadores aritimeticos

---
+++ operadores_aritimeticos.py
@@ -0,0 +1,26 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)  # Soma    
+print(produto_1 - produto_2)  # Subtração
+print(produto_1 * produto_2)  # Multiplicação
+print(produto_1 / produto_2)  # Divisão
+print(produto_1 // produto_2) # Divisão inteira
+print(produto_1 % produto_2)  # Resto da divisão
+print(produto_1 ** produto_2) # Potenciação
+
+
+x = 10
+y = 3      
+
+print(x + y)  # Soma
+print(x - y)  # Subtração
+print(x * y)  # Multiplicação
+print(x / y)  # Divisão
+print(x // y) # Divisão inteira
+print(x % y)  # Resto da divisão
+print(x ** y) # Potenciação
+
+print(10 + 5 * 2)  # A multiplicação tem precedência sobre a adição
+print((10 + 5) * 2)  # Os parênteses alteram a ordem de avaliação, a adição é feita antes da multiplicação
+
